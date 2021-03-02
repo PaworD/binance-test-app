6c13ddba76d7b3f6e89c8c03ed92b1931a6b12c7
Airon <airon@tark.pro>
2021-03-02 19:05:40 +0700

    app.dart: add api by provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,23 @@
+import 'package:binance_test_task/src/resources/binance_api_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './widgets/tickers.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
+    return Provider<BinanceApi>(
+      create: (c) => BinanceApi(),
+      child: MaterialApp(
+        theme: ThemeData(
           fontFamily: "Titillium",
           backgroundColor: Color(0xff212630),
-          brightness: Brightness.dark),
-      home: Scaffold(body: TickersList()),
+          brightness: Brightness.dark,
+        ),
+        home: Scaffold(
+          body: TickersList(),
+        ),
+      )
     );
   }
 }
